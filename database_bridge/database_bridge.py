2c0b0bb76238d5245666157725baf90c960360f8
maximodipalma25-glitch <maximodipalma25@gmail.com>
2026-09-08 22:09:41 -0300

    Lalo:

---
+++ database_bridge/database_bridge.py
@@ -0,0 +1,7 @@
+from database import get_db_connection
+
+
+####################################################
+##   IMPLEMENTAR LALO, TODO POSIBLE GET DE LA BASE DE DATOS QUE SE PUEDA QUERER HACER.
+##   TENES EJEMPLO DE COMO SE USA LA CONEXION EN EL ARCHIVO DE AUTH_SERVICES.PY
+##################################################
