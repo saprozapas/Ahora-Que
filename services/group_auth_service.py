493c5b84e980e2c80aebae55674d95af511c88d5
maximodipalma25-glitch <maximodipalma25@gmail.com>
2026-09-10 12:39:08 -0300

    Corrijo creación de grupos

--- services/group_auth_service.py
+++ services/group_auth_service.py
@@ -11,34 +11,30 @@ class GroupAuthService:
             """
             INSERT INTO public."Grupos" ("Creado_Por", "Nombre", "Descripcion")
             VALUES (%s, %s, %s)
-            RETURNING "Id_Grupo"
             """,
             (user_id, group.get_name(), group.get_descripcion())
-        )
-        group_id = cursor.fetchone()[0]
-        
-        self.add_user(user_id, group_id)
+        )        
 
         conn.commit()
 
         cursor.close()
         conn.close()
 
-    def add_user(self, user_id, group_id):
-    
+    def add_user(self, user_id, group_id, conn=None):
+        if conn is None:
             conn = get_db_connection()
-            cursor = conn.cursor()
+        cursor = conn.cursor()
     
-            cursor.execute(
-                """
-                INSERT INTO public."Usuario-Grupo" ("Usuario_id", "Grupo_id")
-                VALUES (%s, %s)
-                """,
-                (user_id, group_id)
-            )
-            
-    
-            conn.commit()
-    
-            cursor.close()
-            conn.close()
+        cursor.execute(
+            """
+            INSERT INTO public."Usuario-Grupo" ("Usuario_id", "Grupo_id")
+            VALUES (%s, %s)
+            """,
+            (user_id, group_id)
+        )
+        
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
